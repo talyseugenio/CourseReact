a716c3ed578062624079cea450d6d7719f811361
Talys Eugênio <talysebf@gmail.com>
2022-07-21 19:10:15 -0300

    class 7 finished

--- 7_REQ_HTTP_REACT/httpreact/src/hooks/useFetch.js
+++ 7_REQ_HTTP_REACT/httpreact/src/hooks/useFetch.js
@@ -1,59 +1,109 @@
-import {useState, useEffect} from 'react'
+import { useEffect, useState } from "react";
 
-// Customizando hook
-
-
+// 4 - custom hook
 export const useFetch = (url) => {
-    const [data, setData] = useState(null)
+  const [data, setData] = useState(null);
 
-    //refatorando o POST
-    const [config, setConfig] = useState(null)
-    const [method, setMethod] = useState(null)
-    const [callFetch, setCallFetch] = useState(false)
+  // 5 - refatorando post
+  const [config, setConfig] = useState(null);
+  const [method, setMethod] = useState(null);
+  const [callFetch, setCallFetch] = useState(false);
 
-    const httpConfig = (data, method) => {
-        if (method === 'POST'){
-            setConfig({
-                method, 
-                headers: {
-                    'Content-type': 'applications/json'
-                }, 
-                body: JSON.stringify(data)
-            })
-            setMethod(method)
-        }
+  // 6 - estado de loading
+  const [loading, setLoading] = useState(false);
+
+  // 8 - tratando erros
+  const [error, setError] = useState(false);
+
+  // 9 - desafio
+  const [itemId, setItemId] = useState(null);
+
+  const httpConfig = (data, method) => {
+    if (method === "POST") {
+      setConfig({
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      setMethod("POST");
+    } else if (method === "DELETE") {
+      setConfig({
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      setMethod("DELETE");
+      setItemId(data);
     }
+  };
 
-    useEffect (() =>{
-        const fetchData = async () =>{
-            const res = await fetch(url)
+  useEffect(() => {
+    const fetchData = async () => {
+      // 6 - estado de loading
+      // mudar throttling no network p 3g
+      setLoading(true);
 
-            const json = await res.json()
+      // 8 - tratando erros
+      try {
+        const res = await fetch(url);
 
-            setData(json)
+        const json = await res.json();
 
-        }
+        setData(json);
 
-        fetchData()
+        setMethod(null);
 
-    }, [url, callFetch])
+        // 8 - tratando erros
+        setError(null);
+      } catch (error) {
+        console.log(error.message);
 
-    //refatorando POST
-    useEffect(() => {
+        setError("Houve um erro ao carregar os dados!");
+      }
 
-        const httpRequest = async () => {
-            if(method === 'POST'){
-                let fetchOptions = [url, config];
-    
-                const res = await fetch(...fetchOptions);
-        
-                const json = await res.json();
-    
-                setCallFetch(json)
-            }
-        }
-        httpRequest()
-    }, [config, method, url])
+      setLoading(false);
+    };
 
-    return {data, httpConfig}
-}
+    fetchData();
+  }, [url, callFetch]);
+
+  // 5 - refatorando post
+  useEffect(() => {
+    const httpRequest = async () => {
+      if (method === "POST") {
+        // 7 - loading no post
+        // mudar throttling no network p 3g
+        setLoading(true);
+
+        // 5 - refatorando post
+        let fetchOptions = [url, config];
+
+        const res = await fetch(...fetchOptions);
+
+        const json = await res.json();
+
+        setCallFetch(json);
+        // 9 - desafio
+      } else if (method === "DELETE") {
+        const deleteUrl = `${url}/${itemId}`;
+
+        const res = await fetch(deleteUrl, config);
+
+        const json = await res.json();
+
+        setCallFetch(json);
+      }
+    };
+
+    httpRequest();
+  }, [config]);
+
+  console.log(config);
+
+  return { data, httpConfig, loading, error };
+};
